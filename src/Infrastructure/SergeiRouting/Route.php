ace40f4e87329fa1cd69f39ffb686f53ce9f8178
notsoyoung2 <p.sergey1997@mail.ru>
2023-06-02 16:16:43 +0600

    my routing

---
+++ src/Infrastructure/SergeiRouting/Route.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terricon\Forum\Infrastructure\SergeiRouting;
+
+use Terricon\Forum\Infrastructure\Routing\RouteInterface;
+
+class Route implements RouteInterface
+{
+    public function __construct(
+        private readonly string $controller,
+        private readonly string $action,
+        private readonly array $parameters
+    ) {
+    }
+
+    public function getController(): string
+    {
+        return $this->controller;
+    }
+
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+}
