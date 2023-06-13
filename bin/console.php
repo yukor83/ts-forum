99c2a3278ebf990f7439390c638b0ad64a4e6cb6
Korytnikov Yuriy <yu_korytnikov@mail.ru>
2023-06-13 23:51:58 +0600

    commit-1

--- bin/console.php
+++ bin/console.php
@@ -4,7 +4,5 @@ declare(strict_types=1);
 
 require_once '../../vendor/autoload.php';
 
-for($i = 0; $i < 10; $i++) {
-    print "Hello, world!\n";
-    sleep(1);
-}
+$dsn = "mysql:localhost;dbname="
+$pdo = new PDO()
