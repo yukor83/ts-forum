dd2243db933693face6be62ca7bd5f45c317493b
Korytnikov Yuriy <yu_korytnikov@mail.ru>
2023-06-15 00:00:16 +0600

    some-changes-2

--- bin/console.php
+++ bin/console.php
@@ -1,8 +1,47 @@
 #!/usr/bin/php
 <?php
 declare(strict_types=1);
-
+ 
+use Faker\Factory;
+use Faker\Generator;
+ 
 require_once '../../vendor/autoload.php';
-
-$dsn = "mysql:localhost;dbname="
-$pdo = new PDO()
+ 
+$dbServer = 'localhost';
+$dbUser = 'root';
+$dbPass = '12345';
+ 
+try {
+    $dbForum = new PDO("mysql:host=".$dbServer, $dbUser, $dbPass);
+    $dbForum->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    $sql = file_get_contents('../sql/create_datebase_forums.sql');
+    $dbForum->exec($sql);
+    echo "База данных создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_forums.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Forums создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_messages.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Messages создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_topics.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Topics создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_users.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Users создана!";
+    
+    $this->faker = Factory::create();
+    
+    
+    }
+catch (PDOException $e)
+    {
+        echo $sql ."<br>". $e->getMessage();
+    }
+ 
+$dbForum = null;
--- public/index.php
+++ public/index.php
@@ -5,7 +5,42 @@ declare(strict_types=1);
 use Terricon\Forum\Infrastructure\YukorRouting\Router;
 use Terricon\Forum\Infrastructure\ServiceContainer;
 
+use Faker\Factory;
+use Faker\Generator;
+
 require_once '../vendor/autoload.php';
+
+
+$dbServer = 'localhost';
+$dbUser = 'root';
+$dbPass = '12345';
+
+$dbForum = new PDO("mysql:host=".$dbServer, $dbUser, $dbPass);
+$dbForum->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    $sql = file_get_contents('../sql/create_database_forum.sql');
+    $dbForum->exec($sql);
+    echo "База данных создана!";
+
+    $sql = file_get_contents('../sql/create_table_forums.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Forums создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_messages.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Messages создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_topics.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Topics создана!";
+ 
+    $sql = file_get_contents('../sql/create_table_users.sql');
+    $dbForum->exec($sql);
+    echo "Таблица Users создана!";
+    
+    $this->faker = Factory::create();
+
+ $dbForum = null;
 
 $routes = require_once '../config/routes.php';
 $services = require_once '../config/services.php';
@@ -14,3 +49,4 @@ $route = $router->getRoute($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 $serviceContainer = new ServiceContainer($services);
 $controller = $serviceContainer->getInstanceOf($route->getController());
 $controller->{$route->getAction()}(...$route->getParameters());
+
