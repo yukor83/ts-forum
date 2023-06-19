177e175dd83697d2025b6a799c0e01c074ca81fd
Korytnikov Yuriy <yu_korytnikov@mail.ru>
2023-06-19 23:33:37 +0600

    fix-file-path

--- bin/console.php
+++ bin/console.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Faker\Factory;
 use Faker\Generator;
  
-require_once '../../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $faker = Factory::create('ru_RU');
 
@@ -16,17 +16,17 @@ $dbPass = '12345';
     try {
         $dbForum = new PDO("mysql:host=localhost;dbname=forum", $dbUser, $dbPass);
         $dbForum->exec('DROP DATABASE forum;');
-        $sql = file_get_contents('../sql/create_database_forum.sql');
+        $sql = file_get_contents(__DIR__.'/../sql/create_database_forum.sql');
         $dbForum->exec($sql);
-        echo "База данных удалена и создана заново!</br>";
+        echo "База данных удалена и создана заново!\n";
     } catch (PDOException $e) {
         $dbForum = new PDO("mysql:host=localhost", $dbUser, $dbPass);
-        $sql = file_get_contents('../sql/create_database_forum.sql');
+        $sql = file_get_contents(__DIR__.'/../sql/create_database_forum.sql');
         $dbForum->exec($sql);
-        echo "Создана новая базаданных forum!</br>";
+        echo "Создана новая базаданных forum!\n";
     }
 
-    $sql = file_get_contents('../sql/create_table_users.sql');
+    $sql = file_get_contents(__DIR__.'/../sql/create_table_users.sql');
     $dbForum->exec($sql);
     for ($i = 1; $i < 11; $i++)
     {
@@ -34,10 +34,10 @@ $dbPass = '12345';
         $sql = "INSERT users(username, created_at) VALUES ('$username', DEFAULT)";
         $dbForum->exec($sql);
     }
-    echo "Таблица Users создана и заполнена!</br>";
+    echo "Таблица Users создана и заполнена! \n";
 
 
-    $sql = file_get_contents('../sql/create_table_forums.sql');
+    $sql = file_get_contents(__DIR__.'/../sql/create_table_forums.sql');
     $dbForum->exec($sql);
     for ($i = 1; $i < 11; $i++)
     {
@@ -46,9 +46,9 @@ $dbPass = '12345';
         $sql = "INSERT forums(name, description, created_at) VALUES ('$name', '$description', DEFAULT)";
         $dbForum->exec($sql);
     }
-    echo "Таблица Forums создана и заполнена!</br>";
+    echo "Таблица Forums создана и заполнена! \n";
  
-    $sql = file_get_contents('../sql/create_table_topics.sql');
+    $sql = file_get_contents(__DIR__.'/../sql/create_table_topics.sql');
     $dbForum->exec($sql);
     for ($i = 1; $i < 11; $i++)
     {
@@ -56,15 +56,15 @@ $dbPass = '12345';
         {
             $forum_id = $i;
             $user_id = rand(1, 10);
-            $title = $name = $faker->realText($maxNbChars = 20, $indexSize = 1);
+            $title = $faker->realText($maxNbChars = 20, $indexSize = 1);
             $description = $faker->realText($maxNbChars = 100, $indexSize = 1);
             $sql = "INSERT topics(forum_id, user_id, title, description, created_at) VALUES ('$forum_id', '$user_id', '$title', '$description', DEFAULT);";
             $dbForum->exec($sql);
         }
     }
-    echo "Таблица Topics создана и заполнена!</br>";
+    echo "Таблица Topics создана и заполнена! \n";
 
-    $sql = file_get_contents('../sql/create_table_messages.sql');
+    $sql = file_get_contents(__DIR__.'/../sql/create_table_messages.sql');
     $dbForum->exec($sql);
     for ($i = 1; $i < 31; $i++)
     {
@@ -77,6 +77,6 @@ $dbPass = '12345';
             $dbForum->exec($sql);
         }
     }
-    echo "Таблица Messages создана и заполнена!</br>";
+    echo "Таблица Messages создана и заполнена! \n";
 
     $dbForum = null;
